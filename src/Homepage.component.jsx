193215390597ce830d941676f9c2c634fe2fa9d7
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-15 15:43:41 -0700

    Implementation of new feature to keep up with my component

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='homepage'></div>
+    <div className='directory-menu'></div>
+    <div className='menu.item'></div>
+    <div className='homepage'></div>
+    <div className='homepage'></div>
+  )
+};
+
+export default Homepage;
